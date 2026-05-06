2e7d8028298b89c57916d3c123af93655333c9d3
Vimal290704 <vimalyadavkr001@gmail.com>
2026-05-06 23:11:41 +0530

    chore(backend): configure cors and security headers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
 import { askQuestion } from "./controllers/askController";
 import { uploadDocument } from "./controllers/uploadController";
 import { upload } from "./middleware/upload";
@@ -9,6 +11,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
